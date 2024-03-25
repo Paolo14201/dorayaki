1071982d6c30ce15c5a73862dbfbf4e16acd1654
Paolo Venditti <Paolo@MacBook-Pro-di-Paolo.local>
2024-03-25 12:19:26 +0100

    primamodificadorayaki

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <header>
+    <nav className="navbar navbar-expand-lg bg-red navbar-dark">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Progetto</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Manga</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Recensioni</a>
+        </li>
+      </ul>
     </div>
+  </div>
+</nav>
+</header>
+<div className ="container-titolo">
+  <h2> Web app personale</h2>
+  <p className="paragrafo">Prova Testo</p>
+</div>
+</>
   );
 }
 
